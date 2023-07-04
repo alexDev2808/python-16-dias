3125cf53a1cee90203b12b773533f3debbe04876
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-04 12:39:27 -0600

    Trabajando con booleans en Python

---
+++ Dia_3/booleans.py
@@ -0,0 +1,15 @@
+numero = 4 > 5 + 6
+
+print(numero)
+
+mayor_edad = True
+
+print(mayor_edad)
+
+licencia = bool()
+print(licencia)
+
+lista = [4,5,2,6,3,5,6]
+control = 6 in lista
+
+print(control)
