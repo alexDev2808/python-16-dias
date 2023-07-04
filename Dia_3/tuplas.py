66c52102446c1875af78f643a11aa27d36a6ed99
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-04 12:15:37 -0600

    Trabajando con tuplas en Python

---
+++ Dia_3/tuplas.py
@@ -0,0 +1,17 @@
+weekdays = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'sunday')
+
+concert_day = weekdays[4]
+print(concert_day)
+
+to_list = list(weekdays)
+print(type(to_list))
+print(to_list)
+
+weekend = ('Concert', 'Soccer')
+
+saturday, sunday = weekend
+
+print(saturday, sunday)
+
+print(weekdays.count('sunday'))
+print(weekdays.index('friday'))
