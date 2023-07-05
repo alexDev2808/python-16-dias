abf105fd29470fa6b3a670e8e3a3d1125f3ded0a
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-04 18:39:44 -0600

    Mini cambios en el proyecto del dia 3

--- Dia_3/analizar_texto.py
+++ Dia_3/analizar_texto.py
@@ -5,13 +5,20 @@ python = {
 }
 
 texto_ingresado = input("Ingresa un texto: ").lower()
+
 letra_1 = input("Ingresa la primer letra: ").lower()
 letra_2 = input("Ingresa la segunda letra: ").lower()
 letra_3 = input("Ingresa la tercera letra: ").lower()
 
-apariciones_letra_1 = len(texto_ingresado.split(letra_1)) - 1
-apariciones_letra_2 = len(texto_ingresado.split(letra_2)) - 1
-apariciones_letra_3 = len(texto_ingresado.split(letra_3)) - 1
+# Alternativa para contar la cantidad de aparaciones
+# apariciones_letra_1 = len(texto_ingresado.split(letra_1)) - 1
+# apariciones_letra_2 = len(texto_ingresado.split(letra_2)) - 1
+# apariciones_letra_3 = len(texto_ingresado.split(letra_3)) - 1
+
+apariciones_letra_1 = texto_ingresado.count(letra_1)
+apariciones_letra_2 = texto_ingresado.count(letra_2)
+apariciones_letra_3 = texto_ingresado.count(letra_3)
+
 
 frase_invertida = texto_ingresado.split()
 frase_invertida.reverse()
@@ -25,7 +32,7 @@ La letra: {letra_3} aparece {apariciones_letra_3}
 
 Hay {len(texto_ingresado.split())} palabras en total
 
-La primera letra es: '{texto_ingresado[0]}' y la ultima letra es: '{texto_ingresado[len(texto_ingresado) - 1]}'
+La primera letra es: '{texto_ingresado[0]}' y la ultima letra es: '{texto_ingresado[-1]}'
 
 Frase en orden inverso: {" ".join(frase_invertida)}
 
