87e923f4eab2de6b972f9fe12d1122cde5d2777c
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-08 12:14:21 -0600

    Intro a las funciones

---
+++ Dia_5/funciones.py
@@ -0,0 +1,12 @@
+'''
+Comentario multilinea
+def para crear funciones
+puede recibir parametros
+'''
+
+def saludar_persona(nombre):
+    print(f"Hola {nombre}")
+
+saludar_persona("Alexis")
+saludar_persona("Jesus")
+
