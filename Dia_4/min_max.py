7fa97ceda4966204a0dbf2d179011e81d776fd9b
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-07 22:33:56 -0600

    Min y Max

---
+++ Dia_4/min_max.py
@@ -0,0 +1,14 @@
+menor = min(45, 34,55, 5,1, 43)
+mayor = max(5,3,5,3,53,77, 60, 23)
+lista = [64, 64,73,345, 34, 61, 8, 24]
+nombres = ["alex", "tania", "irma", "delfino"]
+nombre = "JEsus"
+dic = { "b1": 10, "a5": 5}
+print(menor)
+print(mayor)
+print(max(lista))
+print(max(nombres))
+print(min(nombre))
+
+print(min(dic)) # clave
+print(min(dic.values()))
