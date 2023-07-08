3ce206b6707f70f022cf0d898b6fb454f5248695
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-08 14:13:04 -0600

    return

--- Dia_5/funciones.py
+++ Dia_5/funciones.py
@@ -7,6 +7,29 @@ puede recibir parametros
 def saludar_persona(nombre):
     print(f"Hola {nombre}")
 
-saludar_persona("Alexis")
-saludar_persona("Jesus")
+def multiplicar(simbolo, veces):
+    return simbolo * veces
 
+def chequear_3_cifras(lista):
+
+    lista_num_3_cifras = []
+    for elem in lista:
+        if int(elem) in range(100, 1000):
+            lista_num_3_cifras.append(elem)
+        else:
+            pass
+
+    return lista_num_3_cifras
+
+nombre = saludar_persona(input("Escribe tu nombre: "))
+
+print(multiplicar('#',  40))
+
+lista_numeros = input("Escribe una lista de numeros separados por un espacio: ").split(" ")
+
+lista_3_cifras = chequear_3_cifras(lista_numeros)
+
+if lista_3_cifras:
+    print(f"Si hay un numero de 3 cifras es {lista_3_cifras[0]}")
+else:
+    print("No hay :( ")
