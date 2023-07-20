4723e5717c4c0b9557846cb36d16d259923da845
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-20 13:01:20 -0600

    Ejercicios

--- Dia_6/prueba.py
+++ Dia_6/prueba.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 registro_ultima_sesion = ["Federico", "20/12/2021", "08:17:32 hs", "Sin errores de carga"]
 
 file = open('registro.txt', 'a')
@@ -13,3 +15,17 @@ file.close()
 file = open('registro.txt', 'r')
 
 print(file.read())
+
+
+def abrir_leer(file):
+    archivo = open(file)
+    contenido = archivo.read()
+
+    return contenido
+
+
+print(abrir_leer('otro.txt'))
+
+ruta = Path('C:/Users/Usuario/Desktop/Curso Python') / 'Cuestionario Día 6' / 'Pregunta 1'
+carpeta = ruta.parents[5]
+print(carpeta)
