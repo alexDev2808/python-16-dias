8c8140e5134a41906630da814ac3d8bc2b8d1bf7
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-20 23:15:17 -0600

    Terminando el proyecto del dia 6: Gestor de Recetas. En su primer version.

--- Dia_6/prueba.py
+++ Dia_6/prueba.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from shutil import rmtree
 
 registro_ultima_sesion = ["Federico", "20/12/2021", "08:17:32 hs", "Sin errores de carga"]
 
@@ -6,7 +7,6 @@ file = open('registro.txt', 'a')
 
 for index, texto in enumerate(registro_ultima_sesion):
     registro_ultima_sesion[index] = texto + "\t"
-
 
 file.writelines(registro_ultima_sesion)
 
@@ -27,5 +27,10 @@ def abrir_leer(file):
 print(abrir_leer('otro.txt'))
 
 ruta = Path('C:/Users/Usuario/Desktop/Curso Python') / 'Cuestionario Día 6' / 'Pregunta 1'
-carpeta = ruta.parents[5]
+carpeta = ruta.parents[0]
 print(carpeta)
+
+mi_archivo = open("no_existe.txt", 'w')
+mi_archivo.write("Contenido nuevo desde Python")
+
+rmtree('prueba')
