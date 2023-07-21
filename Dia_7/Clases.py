a86e44f5058add2069b9c33dc10b420d11378b27
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-21 13:51:41 -0600

    Tipos de metodos

--- Dia_7/Clases.py
+++ Dia_7/Clases.py
@@ -14,13 +14,35 @@ class Pajaro:
         print("Me llamo {}".format(self.nombre_pajaro))
         print(f"Soy de la especie de {self.especie}")
         print("Pio " * 5)
+        self.volar(30)
 
     def volar(self, metros):
         print(f"El pajaro ha volado {metros} metros")
 
+    def pintar_negro(self):
+        self.color_pajaro = 'negro'
+        print(f"Ahora el pajaro es color {self.color_pajaro}")
+
+
+    # No se puede acceder a los atr de instancia
+    @classmethod
+    def poner_huevos(cls, cantidad):
+        print(f"Puso {cantidad} huevos")
+        cls.alas = False
+
+
+    # No se puede acceder a los atr de clase ni de instancia
+    @staticmethod
+    def mirar():
+        print("El pajaro mira")
+
 
 mi_pajaro = Pajaro("Twitter", 'azul', "Colibri")
 otro_pajaro = Pajaro("Alex", 'verde', "Tucan")
+
+Pajaro.poner_huevos(5)
+
+Pajaro.mirar()
 
 print(mi_pajaro.nombre_pajaro)
 print(otro_pajaro.color_pajaro)
