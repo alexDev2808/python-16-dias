bcf22c2aa4d1ccd0888a7a6acf6747074bd4479e
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-23 14:53:42 -0600

    Herencia Extendida

--- Dia_7/Herencia.py
+++ Dia_7/Herencia.py
@@ -6,16 +6,57 @@ class Animal:
     def nacer(self):
         print("Este animal ha nacido")
 
+    def hablar(self):
+        print("Hola " * 4)
+
 
 class Pajaro(Animal):
-    pass
+
+    def __init__(self,edad, color, altura_vuelo):
+        super().__init__(edad, color)
+        self.altura_vuelo = altura_vuelo
+
+    def hablar(self):
+        print("Pio " * 4)
+
+    def volar(self, metros):
+        print(f"El pajaro vuela {metros} metros")
 
 
 print(Animal.__subclasses__())
 print(Pajaro.__bases__)
 
-piolin = Pajaro(4, "Amarillo")
+piolin = Pajaro(4, "Amarillo", 30)
 
 piolin.nacer()
 
 print(piolin.color)
+
+
+class Madre:
+    def reir(self):
+        print("jaja")
+
+    def hablar(self):
+        print("Que tal???")
+
+
+class Padre:
+    def hablar(self):
+        print("Hola")
+
+
+class Hijo(Madre, Padre):
+    pass
+
+
+class Nieto(Hijo):
+    pass
+
+
+mi_nieto = Nieto()
+mi_nieto.hablar()
+mi_nieto.reir()
+
+# Method Orden Resolution
+print(Nieto.__mro__)
