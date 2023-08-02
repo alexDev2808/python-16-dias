33af4d8ac3ddc04e6e0ffd269785e374c2e444cd
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-01 18:16:33 -0600

    Practicando con Pylint

---
+++ Dia_8/practica_pylint.py
@@ -0,0 +1,15 @@
+'''
+Practica con pylint
+'''
+
+
+def sumar(numero1, numero2):
+    '''
+        Funcion sumar
+    '''
+    return numero1 + numero2
+
+
+SUMA = sumar(5, 7)
+
+print(f"El resultado es: {SUMA}")
