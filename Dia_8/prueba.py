1a499e2b6be27992c86a03e7ff601459117f1058
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-01 18:30:16 -0600

    Utilizando unittest

---
+++ Dia_8/prueba.py
@@ -0,0 +1,15 @@
+import unittest
+import cambia_texto
+
+
+class ProbarCambiaTexto(unittest.TestCase):
+
+    def test_mayusculas(self):
+        palabra = 'buen dia'
+        resultado = cambia_texto.todo_mayusculas(palabra)
+        self.assertEqual(resultado, 'Buen Dia')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
