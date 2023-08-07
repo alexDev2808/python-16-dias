f9c3e3ab0db8d15b1a722bf9c27ddc33703def99
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-07 17:21:37 -0600

    Modulo Math

---
+++ Dia_9/modulo_math.py
@@ -0,0 +1,19 @@
+import math
+
+result = math.floor(49.6653)
+print(result)
+
+result = math.ceil(49.1653)
+print(result)
+
+result = math.pi
+print(result)
+
+result = math.log(100, 5)
+print(result)
+
+result = math.tan(3464)
+print(result)
+
+result = math.cos(2535)
+print(result)
