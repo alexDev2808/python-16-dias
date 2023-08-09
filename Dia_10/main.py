b9afd8d0e4a91377cf81aa79b1dc5b63e4f6126b
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-09 11:04:40 -0600

    Agregando al jugador

--- Dia_10/main.py
+++ Dia_10/main.py
@@ -11,14 +11,27 @@ pygame.display.set_caption("Invasion Espacial")
 icono = pygame.image.load('alien.png')
 pygame.display.set_icon(icono)
 
+
+# Jugador
+img_jugador = pygame.image.load("rocket.png")
+jugador_x = 368
+jugador_y = 536
+
+
+def jugador():
+    pantalla.blit(img_jugador, (jugador_x, jugador_y))
 # Loop de juego
 se_ejecuta = True
 
 while se_ejecuta:
+    # rgb
+    pantalla.fill((205, 144, 228))
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
 
-    pantalla.fill((205, 144, 228))
+    jugador()
+
     pygame.display.update()
 
