2ffc13a9ed3e2f903bf23cd387040a29ce10a274
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-09 10:54:32 -0600

    Cambiar titulo, icono y color

--- Dia_10/main.py
+++ Dia_10/main.py
@@ -1,13 +1,24 @@
 import pygame
 
+# Inicializar a pygame
 pygame.init()
 
+# Crear la pantalla
 pantalla = pygame.display.set_mode((800, 600))
 
+# Titulo e icono
+pygame.display.set_caption("Invasion Espacial")
+icono = pygame.image.load('alien.png')
+pygame.display.set_icon(icono)
+
+# Loop de juego
 se_ejecuta = True
 
 while se_ejecuta:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
+
+    pantalla.fill((205, 144, 228))
+    pygame.display.update()
 
