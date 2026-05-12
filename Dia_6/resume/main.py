722638fe881aeff8005bc2e65d8bea60e2632d6b
J Alexis <colores.2808.8000@gmail.com>
2026-05-12 13:24:56 -0600

    resume: Dia 6 -> Escribir archivos(sobreescribir y agregar)

--- Dia_6/resume/main.py
+++ Dia_6/resume/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 """Abrir y manipular archivos"""
 
-def main():
+def leer_archivo():
     # Obtener la ruta del archivo en la misma carpeta que este script
     archivo_path = Path(__file__).parent / "file_1.txt"
     mi_archivo = open(archivo_path, "r", encoding="utf-8")  # Abre con encoding UTF-8
@@ -17,6 +17,23 @@ def main():
 
 
     mi_archivo.close()  # Cierra el archivo
+
+def escribir_archivo(texto:str | None = None):
+    archivo_path = Path(__file__).parent / "file_2.txt"
+    # "w" para escribir (sobrescribe el archivo), "a" para agregar al final del archivo
+    mi_archivo = open(archivo_path, "a", encoding="utf-8")  # Abre el archivo en modo de escritura (append)
+    if texto:
+        mi_archivo.write(texto)
+    else:
+        mi_archivo.write("Hola Mundo!")
+        mi_archivo.write("\n... desde Python!")
+
+    mi_archivo.close()
+    
+def main():
+    leer_archivo()
+    escribir_archivo()
+    escribir_archivo("Me encanta Python, es genial!")  # Agrega texto al archivo
 
 
 if __name__ == "__main__":
